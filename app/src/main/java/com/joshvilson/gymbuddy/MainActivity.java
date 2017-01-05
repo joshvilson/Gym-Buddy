8f44d71c8267f3339ee814bd72eee8164f3f5e84
Josh Vilson <joshvilson96@gmail.com>
2017-01-05 15:00:14 -0500

    initial commit

--- app/src/main/java/com/joshvilson/gymbuddy/MainActivity.java
+++ app/src/main/java/com/joshvilson/gymbuddy/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // made a change example.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
